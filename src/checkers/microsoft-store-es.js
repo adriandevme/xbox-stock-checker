2f955fa1a6d752cf0ce8fb4979df1f7cbd221da8
Adrian Martinez Martinez <adriandev.me@gmail.com>
2021-12-27 09:18:45 +0100

    first working version

---
+++ src/checkers/microsoft-store-es.js
@@ -0,0 +1,6 @@
+const logger = require("../utils/log");
+const microsoftStoreChecker = require("./microsoft-store");
+
+module.exports.checkStock = async function (page, product) {
+  return microsoftStoreChecker.checkStock(page, product, "ES");
+};
---
+++ src/products/microsoft-store.js
@@ -0,0 +1,59 @@
+/*
+  Product Model
+*/
+// url      Product URL    - The url of the product
+// name     Product Name   - Name of the product
+// siteUrl  Site URL       - URL of the product in the store
+// siteName Site name      - Name of the site
+// checker  Checker        - Checker (name) to use
+
+module.exports = [
+  //Xbox Series X - DE
+  {
+    url: "https://www.xbox.com/de-de/configure/8WJ714N3RBTL",
+    name: "Xbox Series X",
+    siteUrl: '"https://www.xbox.com/de-de',
+    siteName: "Microsoft Store Alemania",
+    checker: "microsoft-store-de.js",
+  },
+  //Xbox Series X - ES
+  {
+    url: "https://www.xbox.com/es-es/configure/8WJ714N3RBTL",
+    name: "Xbox Series X",
+    siteUrl: '"https://www.xbox.com/es-es',
+    siteName: "Microsoft Store España",
+    checker: "microsoft-store-es.js",
+  },
+  //Xbox Series X - FR
+  {
+    url: "https://www.xbox.com/fr-fr/configure/8WJ714N3RBTL",
+    name: "Xbox Series X",
+    siteUrl: '"https://www.xbox.com/fr-fr',
+    siteName: "Microsoft Store Francia",
+    checker: "microsoft-store-fr.js",
+  },
+  //Xbox Series X - IT
+  {
+    url: "https://www.xbox.com/it-it/configure/8WJ714N3RBTL",
+    name: "Xbox Series X",
+    siteUrl: '"https://www.xbox.com/it-it',
+    siteName: "Microsoft Store Italia",
+    checker: "microsoft-store-it.js",
+  },
+  //Xbox Series X - PL
+  {
+    url: "https://www.xbox.com/pl-pl/configure/8WJ714N3RBTL",
+    name: "Xbox Series X",
+    siteUrl: '"https://www.xbox.com/pl-pl',
+    siteName: "Microsoft Store Polonia",
+    checker: "microsoft-store-pl.js",
+  },
+  // //Xbox Series S - PL
+  // {
+  //   url: "https://www.xbox.com/es-es/configure/942J774TP9JN",
+  //   name: "Xbox Series S",
+  //   siteUrl: '"https://www.xbox.com/es-es',
+  //   siteName: "Microsoft Store Espana",
+  //   checker: "microsoft-store-es.js",
+  // },
+];
